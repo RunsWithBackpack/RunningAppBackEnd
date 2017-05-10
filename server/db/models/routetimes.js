51cbe1c991f3e6f60139c8b4470f6e1c733e2e3a
gklapman <gabiklapman@gmail.com>
2017-05-10 12:41:04 -0500

    fixes best

--- server/db/models/routetimes.js
+++ server/db/models/routetimes.js
@@ -31,8 +31,25 @@ var Routetime = db.define('routetime',
   {
     getterMethods : {
       runtime: function(){
+        console.log('runtime getter method ', this.personalTimeMarker[this.personalTimeMarker.length-1]-this.personalTimeMarker[0])
         return this.personalTimeMarker[this.personalTimeMarker.length-1]-this.personalTimeMarker[0];
       },
+      // best: function(){
+      //   let userId = this.userId
+      //   let routeId = this.routeId
+      //   return Routetime.findAll({where: {userId, routeId}})
+      //   .then(routetimes => {
+
+      //     let sortedRows = routetimes.sort(function(a,b){return b.runtime-a.runtime})
+      //     console.log('sorted ', sortedRows)
+      //     if (this === sortedRows[0]){
+      //       return true
+      //     } else {
+      //       return false
+      //     }
+      //     // routetimes.runthis.personalTimeMarker[this.personalTimeMarker.length-1]
+      //   })
+      // }
     },
       setterMethods : {
       jsonLatLongCoords: function(jsonLatLongArr){
@@ -46,19 +63,38 @@ var Routetime = db.define('routetime',
     },
     hooks: {
       afterCreate: function(newroutetime) {
+        console.log('THIS IS RUNNING')
         //this hook ensures 2 things:
         //1: that a players routetimes (for a given route) are updated with the correct boolean for the "best" field after a new routetime is created (for that route and player)
         //2: that the associated route for the newly created routetime has its popularity field updated (incremented by 1)
 
         userId= newroutetime.userId;
         routeId= newroutetime.routeId;
+        console.log('user id ', userId, 'route Id', routeId)
 
         let upRoutetimeBestProm=
-        Routetime.update({best: false},{where: {userId, routeId}, returning: true})//not sure why i may need return, but im doing it anyway... also, remember you NEED returning: true, if you want the second element of the resolved value (updatedRes) to contain the updated rows
-          .then((updatedRes)=>{
-            let numaffectedRows= updatedRes[0];//just for future reference
-            let affectedRows= updatedRes[1];//affectedRows are the updated routetimes (associated with a user and route)
-            return affectedRows.sort(function(a,b){return b.runtime-a.runtime})[0].update({best: true});//this sets the best routetime for a particular user and route to have "true" value for its field "best"
+        // Routetime.update({best: false},{where: {userId, routeId}, returning: true})//not sure why i may need return, but im doing it anyway... also, remember you NEED returning: true, if you want the second element of the resolved value (updatedRes) to contain the updated rows
+          // .then((updatedRes)=>{
+          //   console.log('updatedRes', updatedRes)
+          //   let numaffectedRows= updatedRes[0];//just for future reference
+          //   let affectedRows= updatedRes[1];//affectedRows are the updated routetimes (associated with a user and route)
+          //   let sortedRows = affectedRows.sort(function(a,b){return b.runtime-a.runtime})[0]//this sets the best routetime for a particular user and route to have "true" value for its field "best"
+          //   sortedRows.update({best: true});
+          //   console.log('sorted rows', sortedRows)
+          //   return sortedRows
+          // })
+
+        Routetime.findAll({where: {userId, routeId}})
+          .then((allroutetimes)=>{
+            console.log('allroutetime length ',allroutetimes.length)
+            return Promise.all(allroutetimes.map(routetime=>routetime.update({best: false})))
+          })
+          .then((updatedroutetimes)=>{
+            console.log('updatedRes length', updatedroutetimes.length)
+            let bestroutetime = updatedroutetimes.sort(function(a,b){return a.runtime-b.runtime})[0]//this sets the best routetime for a particular user and route to have "true" value for its field "best"
+            bestroutetime.update({best: true});
+            console.log('sorted rows', sortedRows)
+            return bestroutetime
           })
           .catch(err=>err);
 
@@ -68,7 +104,7 @@ var Routetime = db.define('routetime',
             return associatedRoute.increment({'popularity': 1})
           })
           .catch(err=>err);
-
+      
         return Promise.all([upRoutetimeBestProm, upRoutePopularityProm]);
       },
     }
