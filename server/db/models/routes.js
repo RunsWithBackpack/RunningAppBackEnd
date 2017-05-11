98a85dd4ea6f7ec6cc32a4301db030fd47c9c018
Charles <charles.m.long43@gmail.com>
2017-05-11 12:03:12 -0500

    fixed checkpoints

--- server/db/models/routes.js
+++ server/db/models/routes.js
@@ -30,7 +30,8 @@ var Route = db.define('route',
       },
       checkpointCoords: function(){
         return this.coords.filter(coordPair => {
-          return this.coords.indexOf(coordPair) % 10 === 0
+          if(this.coords.indexOf(coordPair) % 5 === 0) return true
+          else if(this.coords.indexOf(coordPair) === this.coords.length-1) return true
         })
       }
       // popularity: function(){    //unfortunatley this returns a promise, and not the length itself... this can be implemented again if we find a way around this.. but for now, popularity will be a direct field of route, updated by routetimes aftercreate hook
