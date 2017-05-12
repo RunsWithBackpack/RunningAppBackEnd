be0dd88d0fe2b96a2e5d48fb4a6941c00c9dac20
Charles <charles.m.long43@gmail.com>
2017-05-12 12:10:48 -0500

    added test

---
+++ server/app/test/test.spec.js
@@ -0,0 +1,20 @@
+var dbIndex = require('../db');
+var db= dbIndex.db;
+
+import chai, {expect} from 'chai';
+
+describe('TEST', () => {
+
+  describe('should asdfasdf', () => {
+    var a= 2
+    beforeEach('asdf', () => {
+    });
+
+    it('asdf', () => {
+      expect(a+a).to.be.equal(4)
+
+    });
+
+  });
+
+});
