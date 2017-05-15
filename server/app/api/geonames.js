4827c0b4ce372429cac4b9432889a47e47748b3f
Charles <charles.m.long43@gmail.com>
2017-05-15 10:49:54 -0500

    geonames changed

--- server/app/api/geonames.js
+++ server/app/api/geonames.js
@@ -4,9 +4,10 @@ const axios=require('axios')
 module.exports = require('express').Router()
   .get('/',
     (req, res, next) => {
+      let username= (!req.query.username) ? 'demo' : req.query.username
       let latitude= req.query.latitude
       let longitude= req.query.longitude
-      return axios.get(`http://api.geonames.org/findNearestIntersectionJSON?lat=${latitude}&lng=${longitude}&username=demo`)
+      return axios.get(`http://api.geonames.org/findNearestIntersectionJSON?lat=${latitude}&lng=${longitude}&username=${username}`)
         .then(res=>{
           return res.data.intersection})
         .then(intersection=>{
@@ -17,12 +18,13 @@ module.exports = require('express').Router()
       })
   .get('/bulk',
     (req, res, next) => {
-      let max= (!req.query.max) ? 4 : req.query.max;//default max if none was specified
+      let max= (!req.query.max) ? 8 : req.query.max//default max if none was specified
+      let username= (!req.query.username) ? 'demo' : req.query.username
       // console.log(max)
       let latitude= req.query.latitude
       let longitude= req.query.longitude
       console.log('req.query is ', req.query)
-      return axios.get(`http://api.geonames.org/findNearestIntersectionJSON?lat=${latitude}&lng=${longitude}&maxRows=${max}&username=demo`)
+      return axios.get(`http://api.geonames.org/findNearestIntersectionJSON?lat=${latitude}&lng=${longitude}&maxRows=${max}&username=${username}`)
         .then(response=>{
           console.log('geonames response is ', response)
           let intersectionsArr= response.data.intersection
