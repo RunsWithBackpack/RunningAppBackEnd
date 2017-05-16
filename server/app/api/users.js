bd25a1d2167cd0b644afa9c1b58334ce1eb2a346
gklapman <gabiklapman@gmail.com>
2017-05-16 16:59:25 -0500

    fixes seed and me file

--- server/app/api/users.js
+++ server/app/api/users.js
@@ -16,30 +16,19 @@ const Routetime = dbIndex.Routetime;
 // module.exports = require('express').Router()
 
 
-router.use(session({
-  secret: 'phantomRacer', 
-  resave: false,
-  saveUninitialized: false
-  }));
 
-
- router.use(function (req, res, next) {
-    console.log('session!!', req.session);
-     next();
-  });
-
-  router.get('/',
-    // The forbidden middleware will fail *all* requests to list users.
-    // Remove it if you want to allow anyone to list all users on the site.
-    //
-    // If you want to only let admins list all the users, then you'll
-    // have to add a role column to the users table to support
-    // the concept of admin users.
-    // forbidden('listing users is not allowed'), //Have not implemented this just yet...
-    (req, res, next) =>
-      User.findAll()
-        .then(users => res.json(users))
-        .catch(next))
+ //  router.get('/',
+ //    // The forbidden middleware will fail *all* requests to list users.
+ //    // Remove it if you want to allow anyone to list all users on the site.
+ //    //
+ //    // If you want to only let admins list all the users, then you'll
+ //    // have to add a role column to the users table to support
+ //    // the concept of admin users.
+ //    // forbidden('listing users is not allowed'), //Have not implemented this just yet...
+ //    (req, res, next) =>
+ //      User.findAll()
+ //        .then(users => res.json(users))
+ //        .catch(next))
 
 //This is the route for getting all the routes/routetimes that a user has run
   router.get('/:id', (req, res, next)=> {
@@ -55,6 +44,17 @@ router.use(session({
       })
       .catch((next))
 
+  })
+
+  router.put('/:id', (req, res, next)=> {
+    console.log('HERE with this ', req.body)
+    return User.findOne({where: {id: req.params.id}})
+    .then(user => {
+      return user.update(req.body)
+    })
+    .then(updatedUser => {
+      res.json(updatedUser)
+    })
   })
 
 module.exports = router
