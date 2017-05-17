eabcc505df477ecd68d224f64402853c1ee149a7
gklapman <gabiklapman@gmail.com>
2017-05-17 09:59:39 -0500

    all users route

--- server/app/api/users.js
+++ server/app/api/users.js
@@ -17,18 +17,18 @@ const Routetime = dbIndex.Routetime;
 
 
 
- //  router.get('/',
- //    // The forbidden middleware will fail *all* requests to list users.
- //    // Remove it if you want to allow anyone to list all users on the site.
- //    //
- //    // If you want to only let admins list all the users, then you'll
- //    // have to add a role column to the users table to support
- //    // the concept of admin users.
- //    // forbidden('listing users is not allowed'), //Have not implemented this just yet...
- //    (req, res, next) =>
- //      User.findAll()
- //        .then(users => res.json(users))
- //        .catch(next))
+  router.get('/',
+    // The forbidden middleware will fail *all* requests to list users.
+    // Remove it if you want to allow anyone to list all users on the site.
+    //
+    // If you want to only let admins list all the users, then you'll
+    // have to add a role column to the users table to support
+    // the concept of admin users.
+    // forbidden('listing users is not allowed'), //Have not implemented this just yet...
+    (req, res, next) =>
+      User.findAll()
+        .then(users => res.json(users))
+        .catch(next))
 
 //This is the route for getting all the routes/routetimes that a user has run
   router.get('/:id', (req, res, next)=> {
